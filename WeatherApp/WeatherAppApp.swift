ddb482bbbeac9902127b3f4de3c3f6df1b94f010
Rejina Luitel <rejina@inficare.com.my>
2025-02-06 10:24:23 +0545

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Rejina Luitel on 06/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppTests/WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTests.swift
+//  WeatherAppTests
+//
+//  Created by Rejina Luitel on 06/02/2025.
+//
+
+import Testing
+@testable import WeatherApp
+
+struct WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
